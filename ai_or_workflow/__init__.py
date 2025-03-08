e42825cc01eaeba907e4a47838bb9dbba7c66d63
CDMonsalveA <cdmonsalvea1@gmail.com>
2025-03-08 15:46:36 -0500

    Add module docstring for AI and Operations Research Workflow

---
+++ ai_or_workflow/__init__.py
@@ -0,0 +1,5 @@
+"""
+Artificial Intelligence and Operations Research Workflow is a series of modules 
+that allow the integration of Algorithms and Models from both fields to solve a 
+real-world problem.
+"""

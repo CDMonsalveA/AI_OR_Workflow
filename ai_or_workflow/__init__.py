07fcb0fe83b7488f9576212a95b0bf00b75fdc2a
CDMonsalveA <cdmonsalvea1@gmail.com>
2025-03-08 19:16:06 -0500

    Fix formatting in module docstring for clarity

--- ai_or_workflow/__init__.py
+++ ai_or_workflow/__init__.py
@@ -1,5 +1,5 @@
 """
-Artificial Intelligence and Operations Research Workflow is a series of modules 
-that allow the integration of Algorithms and Models from both fields to solve a 
+Artificial Intelligence and Operations Research Workflow is a series of modules
+that allow the integration of Algorithms and Models from both fields to solve a
 real-world problem.
 """

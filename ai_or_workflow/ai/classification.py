3e1dafb40df0884793231a4221b89a9145e6d40e
CDMonsalveA <cdmonsalvea1@gmail.com>
2025-03-20 22:27:34 -0500

    Enhance classification module: update docstring and import additional classifiers

--- ai_or_workflow/ai/classification.py
+++ ai_or_workflow/ai/classification.py
@@ -1,1 +1,36 @@
-"""Colection of functions for classification tasks."""
+"""Colection of functions for classification tasks.
+
+Supported models:
+- One Rule (OneR)
+- Naive Bayes (GaussianNB, MultinomialNB, BernoulliNB, CategoricalNB)
+- Decision Tree Classifier
+- Random Forest Classifier
+- Linear Discriminant Analysis (LDA)
+- Logistic Regression
+- Support Vector Classifier (SVC)
+- K-Nearest Neighbors (KNN)
+-"""
+
+# Import clasification functions
+
+# Random Forest Classifier
+from sklearn.ensemble import (
+    BaggingClassifier,
+    GradientBoostingClassifier,
+    GradientBoostingRegressor,
+    RandomForestClassifier,
+    StackingClassifier,
+    VotingClassifier,
+)
+
+# OneR is a simple rule-based classifier that selects the attribute that best separates the classes.
+# Naive Bayes
+from sklearn.naive_bayes import (
+    BernoulliNB,
+    CategoricalNB,
+    GaussianNB,
+    MultinomialNB,
+)
+
+# Decision Tree Classifier
+from sklearn.tree import DecisionTreeClassifier, ExtraTreeClassifier
